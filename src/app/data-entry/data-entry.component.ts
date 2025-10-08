f74daa4f2004b4ab2925b17ebff1a7125a76e338
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-10-08 16:38:18 +0200

    Adds customer data binding and save functionality
    Introduces two-way data binding for customer data, enabling parent components to provide and update draft customer instances.
    
    Implements validation and save functionality in the customer data component, ensuring all fields are complete before emitting saved data.
    
    Enhances data entry components with change detection to propagate updates.

--- src/app/data-entry/data-entry.component.ts
+++ src/app/data-entry/data-entry.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,7 +10,15 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './data-entry.component.scss',
 })
 export class DataEntryComponent {
-  @Input() label!: string;
-  @Input() model!: any;
-  @Input() field!: string;
+  @Input({ required: true }) label!: string;
+  @Input({ required: true }) model!: any;
+  @Input({ required: true }) field!: string;
+
+  @Output() valueChange = new EventEmitter<void>();
+  @Output() commit = new EventEmitter<void>();
+
+  onChange() {
+    this.valueChange.emit();
+  }
+ 
 }
