d3309b2e985d1ee58f1f3b89e5057aa9f565b292
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-10-08 11:20:56 +0200

    Adds CustomerDataComponent to the app structure
    Integrates the CustomerDataComponent into the application by updating the root template and its module imports.
    
    Enhances the app's functionality and prepares the layout to display customer-related data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { CustomerDataComponent } from "./customer-data/customer-data.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CustomerDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
