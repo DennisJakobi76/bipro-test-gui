a328495b436a493a9f7e6e7c00f5b92f64773520
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-10-08 17:25:23 +0200

    Adds CurrentPoliciesComponent with template and styles; updates AppComponent to include it

---
+++ src/app/current-policies/current-policies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-policies',
+  imports: [],
+  templateUrl: './current-policies.component.html',
+  styleUrl: './current-policies.component.scss'
+})
+export class CurrentPoliciesComponent {
+
+}
