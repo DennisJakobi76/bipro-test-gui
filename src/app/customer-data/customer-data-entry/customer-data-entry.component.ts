1c21e16a905cf33690993ff782a405e6a0a3fe6e
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-10-08 12:01:10 +0200

    Enhances customer data UI with layout and styling updates
    Updates the customer data component to include a structured layout
    with a dedicated entry component. Introduces a new SCSS file for
    styling, improving alignment, spacing, and responsive design.
    
    These changes enhance user experience and prepare for future
    feature extensions.

---
+++ src/app/customer-data/customer-data-entry/customer-data-entry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-data-entry',
+  imports: [],
+  templateUrl: './customer-data-entry.component.html',
+  styleUrl: './customer-data-entry.component.scss'
+})
+export class CustomerDataEntryComponent {
+
+}
